66ae949e0ca6fd7f65217975f39c293f62bc9ae1
zsolnai <georg.zsolnai123@gmail.com>
2024-11-04 11:30:19 +0100

    Add 1st version of permute function

--- Assignment1/minhash.py
+++ Assignment1/minhash.py
@@ -27,35 +27,34 @@ class MinHash:
         for _, shingles in self.shingles.collect():
             listOfShingles.update(shingles)
 
-        # I want to create a gloabl set of shingles which will be my rows
-        # then each sentence will be the column: therefore we can see if a given sentence
-        # contains a shingle and then we assign a 1 to create the matrix
-        
-        # create signature matrix from shingle values (rows) and shingle sets (columns)
         sig_matrix = np.empty((len(listOfShingles), self.shingles.count()))
         print(sig_matrix.shape)
 
+        premutation = self.permute(num_hash_functions)
 
-        for row in range(len(listOfShingles)): # for each row
-            for column in range(self.shingles.count()): # for each column
+        for _, shingles in self.shingles.collect():
+            for row in shingles: # for each row
+                for column in range(self.shingles.count()): # for each column
 
-                sig_matrix[row][column] = 0 ## IF we need to show 0 values
-                # Add 1 if shingle is in column (look at self.shinglesp[0].getSetItem(row))
+                    sig_matrix[row][column] = 0 ## IF we need to show 0 values
+                    # Add 1 if shingle is in column (look at self.shinglesp[0].getSetItem(row))
 
-                # shingle = ham: {set} so we need to check if row shingle is in the set, if yes we add 1
-                # then we should have a matrix with 1 and 0 values 
+                    # shingle = ham: {set} so we need to check if row shingle is in the set, if yes we add 1
+                    # then we should have a matrix with 1 and 0 values 
 
-                if list(listOfShingles)[row] in self.shingles.collect()[column]:
-                    sig_matrix[row][column] = 1
+                    if list(listOfShingles)[row] in self.shingles.collect()[column]:
+                        sig_matrix[row][column] = 1
 
         print(sig_matrix)
    
         return
-        # Implement minhashing
-        pass
 
     def permute(self, num_hash_functions):
         # Implement permutation
         # we need to have different combinations 
+        permutation = np.empty((num_hash_functions, self.shingles.count()))
 
-        pass
+        for i in range(num_hash_functions):
+            permute = np.random.rand(self.shingles.count())
+            permutation[i] = permute
+        return permutation
