dfcca6ba7043992e126dfa040287fb3c61614bc0
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2024-11-24 12:54:21 +0100

    doc: add comments

--- Assignment3/streaming_triangles.py
+++ Assignment3/streaming_triangles.py
@@ -26,16 +26,22 @@ class TriangleCounter:
 
         edge_reservoir_updated = False
         # Edge reservoir sampling
+        # If the size of the reservoir is smaller than the fixed size
+        # Then we add it to the reservoir
         if len(self.edge_reservoir) < self.EDGE_RESERVOIR_SIZE:
             if edge not in self.edge_reservoir:
                 self.edge_reservoir.append(edge)
                 edge_reservoir_updated = True
 
+        # If not, depending on the probabilty we add it and mark the ege_reservoir_updated flag to true
+        # If the probability complies, we replace the new edge with an existing one
         elif random.random() <= 1 / self.time:
             random_index = random.randint(0, len(self.edge_reservoir) - 1)
             self.edge_reservoir[random_index] = edge
             edge_reservoir_updated = True
 
+        # If the edge reservoir has been updated then we recalculate the total number of wedges and the new wedges
+        # Due to the new edge reservoir modification
         if edge_reservoir_updated:
             self.total_wedges = self.update_total_wedges()
             new_wedges = self.generate_new_wedges(edge)
@@ -44,6 +50,8 @@ class TriangleCounter:
 
             # Wedge reservoir sampling
             for new_wedge in new_wedges:
+                # If the total_wedges is not 0 and the probability complies we add the wedge to the reservoir
+                # If the wedge reservoir is full we add replace the new wedge with an existing one
                 if self.total_wedges:
                     if random.random() < len(new_wedges) / self.total_wedges:
                         if len(self.wedge_reservoir) < self.WEDGE_RESERVOIR_SIZE:
@@ -68,28 +76,40 @@ class TriangleCounter:
         #print(f"Time: {old_time}, Kappa_t: {kappa_t}, T_t: {T_t}")
 
         return old_time, kappa_t, T_t
+    
+    ### Helper Functions
 
     def is_closed_by(self, edge, wedge):
+        # Returns true if the edge is a subset of the wedge.
+        # In other words, if the edge forms part of the wedge
         return set(edge) <= set(wedge)
 
     def update_total_wedges(self):
         total = 0
+        # Iterate over each edge in the edge reservoir
         for i, (u, v) in enumerate(self.edge_reservoir):
+            # Iterate over each edge in the reservoir + 1
             for j, (x, y) in enumerate(self.edge_reservoir[i+1:]):
+                # Check if edge (u,v) shares a node with edge (x,y)
                 if u in (x, y) or v in (x, y):
+                    # If they share a node we increment the amount of wedges by 1
                     total += 1
         return total
 
     def generate_new_wedges(self, edge):
         node_u, node_v = edge
         new_wedges = []
+        # We iterate over all of the edges in the reservoir
         for other_edge in self.edge_reservoir:
-
+            # If the node we are looping through of the edge in the reservoir is in the incoming edge and not the other node 
             if node_u in other_edge and node_v not in other_edge:
+                # Add the opposite node of the edge not the one that are connecting
                 other_node = other_edge[0] if other_edge[1] == node_u else other_edge[1]
                 new_wedges.append((node_u, node_v, other_node))
 
+            # If the node we are looping through of the edge in the reservoir is in the incoming edge and not the other node 
             elif node_v in other_edge and node_u not in other_edge:
+                # Add the opposite node of the edge not the one that are connecting
                 other_node = other_edge[0] if other_edge[1] == node_v else other_edge[1]
                 new_wedges.append((node_v, node_u, other_node))
 
@@ -139,12 +159,8 @@ def main():
     print("First 20 processed edges:")
     results.show(20, truncate=False)
 
-    print(f"20 edges from the middle (starting at row {total_edges // 2}):")
-    results.orderBy("results.time").limit(20).offset(total_edges // 2).show(truncate=False)
-
     print("Last 20 processed edges:")
     results.orderBy(desc("results.time")).limit(20).show(truncate=False)
-
 
     spark.stop()
 
